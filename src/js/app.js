1ad4b8079d1a553f51f87b13cf14e6f2ca0a7259
Pratik Chauhan <pratik@cixon.de>
2023-01-09 12:48:21 +0530

    GSAP Added

--- src/js/app.js
+++ src/js/app.js
@@ -5,13 +5,48 @@ import MagneticButton from './functions/magnetic-button';
 
 MagneticButton();
 
+gsap.registerPlugin(ScrollTrigger);
 const scroller = new LocomotiveScroll({
     el: document.querySelector('[data-scroll-container]'),
     smooth: true,
-    multiplier: 1
 })
 
-gsap.registerPlugin(ScrollTrigger);
-scroller.on('scroll', ScrollTrigger.update)
-ScrollTrigger.addEventListener('refresh', () => scroller.update())
-ScrollTrigger.refresh()
+
+scroller.on('scroll', ScrollTrigger.update);
+
+ScrollTrigger.scrollerProxy("body", {
+    scrollTop(value) {
+        return arguments.length ?
+            scroller.scrollTo(value, 0, 0) :
+            scroller.scroll.instance.scroll.y;
+    },
+    getBoundingClientRect() {
+        return {
+            top: 0,
+            left: 0,
+            width: window.innerWidth,
+            height: window.innerHeight
+        };
+    }
+});
+
+gsap.from(".pizza_heading", {
+    scrollTrigger: {
+        trigger: ".pizza_section",
+        scroller: "body",
+        scrub: true,
+        pin: true,
+        start: "top top",
+        end: "+=100%"
+    },
+    scaleX: 0,
+    transformOrigin: "left center",
+    ease: "none"
+});
+
+
+
+
+
+ScrollTrigger.addEventListener("refresh", () => scroller.update());
+ScrollTrigger.refresh();
--- src/js/dist/app.dev.js
+++ src/js/dist/app.dev.js
@@ -11,17 +11,44 @@ var _magneticButton = _interopRequireDefault(require("./functions/magnetic-butto
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
 (0, _magneticButton["default"])();
-var scroller = new _locomotiveScroll["default"]({
-  el: document.querySelector('[data-scroll-container]'),
-  smooth: true,
-  multiplier: 1
-});
 
 _gsap["default"].registerPlugin(_ScrollTrigger["default"]);
 
+var scroller = new _locomotiveScroll["default"]({
+  el: document.querySelector('[data-scroll-container]'),
+  smooth: true
+});
 scroller.on('scroll', _ScrollTrigger["default"].update);
 
-_ScrollTrigger["default"].addEventListener('refresh', function () {
+_ScrollTrigger["default"].scrollerProxy("body", {
+  scrollTop: function scrollTop(value) {
+    return arguments.length ? scroller.scrollTo(value, 0, 0) : scroller.scroll.instance.scroll.y;
+  },
+  getBoundingClientRect: function getBoundingClientRect() {
+    return {
+      top: 0,
+      left: 0,
+      width: window.innerWidth,
+      height: window.innerHeight
+    };
+  }
+});
+
+_gsap["default"].from(".pizza_heading", {
+  scrollTrigger: {
+    trigger: ".pizza_section",
+    scroller: "body",
+    scrub: true,
+    pin: true,
+    start: "top top",
+    end: "+=100%"
+  },
+  scaleX: 0,
+  transformOrigin: "left center",
+  ease: "none"
+});
+
+_ScrollTrigger["default"].addEventListener("refresh", function () {
   return scroller.update();
 });
 
